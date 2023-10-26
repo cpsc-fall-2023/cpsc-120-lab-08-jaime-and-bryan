a1eb6b78ea8e77c72a7b1782c588c3720a533248
bryan orozco <bryancostco@csu.fullerton.edu>
2023-10-25 19:35:36 -0700

    working on loops , also need to finish the average

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Bryan Orozco
+// bryancostco@csu.fullerton.edu
+// @Bryancostco
+// Partners: @jaimegalvan23
 
 #include <iostream>
 #include <string>
@@ -6,10 +9,13 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-
+  int i;
   // TODO: Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
-
+if (argc < 1) {
+std::cout << "Error: you must supply at least one argument";
+return 1;
+}
   // TODO: Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
@@ -22,10 +28,10 @@ int main(int argc, char* argv[]) {
   // TODO: After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
-
+double AVERAGE;
   // TODO: Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
-
+std::cout << " average = " << AVERAGE;
   return 0;
 }
