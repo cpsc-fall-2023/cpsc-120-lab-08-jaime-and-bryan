04c8cf3ee620b1974baf7b421a459683aa4188ba
bryan orozco <bryancostco@csu.fullerton.edu>
2023-11-01 18:06:38 -0700

    finally working , worked with for lop as well as catch

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,7 +1,7 @@
 // Bryan Orozco
 // bryancostco@csu.fullerton.edu
 // @Bryancostco
-// Partners: @jaimegalvan23
+// Partners: @jaimegalvan23cd
 
 #include <iostream>
 #include <string>
@@ -9,29 +9,38 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-  int i;
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
-if (argc < 1) {
-std::cout << "Error: you must supply at least one argument";
-return 1;
-}
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
 
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
-double AVERAGE;
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
-std::cout << " average = " << AVERAGE;
+  if (argc <= 1) {
+    std::cerr << "Error: Please provide at least one command line argument."
+              << "\n";
+    return 1;
+  }
+
+  double sum = 0.0;
+  int count = 0;
+
+  for (int i = 1; i < argc; ++i) {
+    try {
+      double num = std::stod(arguments[i]);
+      sum += num;
+      count++;
+    } catch (const std::invalid_argument& e) {
+      std::cerr << "Error: Argument '" << arguments[i]
+                << "' is not a valid number."
+                << "\n";
+      return 1;
+    }
+  }
+
+  if (count == 0) {
+    std::cerr << "Error: No valid numbers found in the arguments."
+              << "\n";
+    return 1;
+  }
+
+  double average = sum / count;
+
+  std::cout << "average = " << average << "\n";
+
   return 0;
 }
